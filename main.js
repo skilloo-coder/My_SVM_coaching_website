7dbc61c6c43ef1665a500ccfcd22fa0487fd2e62
Ayush Gupta <82319063+skilloo-coder@users.noreply.github.com>
2022-11-07 13:38:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log('MAAFI Learning Center');
+
+function myfunction() {
+  var showhide = document.getElementById('mylinks');
+  if(showhide.style.display === "block"){
+    showhide.style.display  = "none";
+  } else{
+    showhide.style.display = "block";
+  }
+}
+
+
+var slack = document.getElementById('preloader');
+function myfun(){
+  preloader.style.display = 'none';
+}
